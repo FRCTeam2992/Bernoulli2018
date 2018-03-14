3eef944f0509468e7b3cf4d4a8b64e8889bcb64f
10413996 <10413996@MAHL1304197006A.stpsb.pri>
2018-03-14 16:36:11 -0500

    made new angled center auto - Carly, Sam, Vani, Kurt

---
+++ src/org/usfirst/frc2992/CubeBert2018/commands/AutoAngleCenterSwitch.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc2992.CubeBert2018.commands;
+
+import org.usfirst.frc2992.CubeBert2018.Robot;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ */
+public class AutoAngleCenterSwitch extends CommandGroup {
+	//for auto stuff do commandgroup so can sequence other commands
+	
+    public AutoAngleCenterSwitch() {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    
+    	addSequential(new AutoDriveFwd(20,0.8,3,true,0));
+    	addSequential(new AutoDriveTurn(-33.27,0.8,2));
+    	addSequential(new AutoDriveFwd(76.55,0.8,8,true,-33.27));
+    	addSequential(new AutoDriveTurn(0,0.8,2));
+    	addParallel(new AutoIntakeReady());
+    	addSequential(new liftHeight(Robot.constants.switchHeight+5,3));
+    	addParallel(new liftStop());
+    	addSequential(new AutoDriveFwd(42,0.8,5,true,0));
+    	addSequential(new intakeOut(1.0));
+    	addSequential(new WaitCommand(1.0));
+    	addSequential(new intakeStop());
+    	
+    }
+}
